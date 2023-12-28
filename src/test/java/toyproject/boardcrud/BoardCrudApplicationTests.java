7147cab91a4ce617c3093ed37981a8829f32598f
haecheol-shin <dev.hc.shin@gmail.com>
2023-12-28 19:06:36 +0900

    first commit

---
+++ src/test/java/toyproject/boardcrud/BoardCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.boardcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
