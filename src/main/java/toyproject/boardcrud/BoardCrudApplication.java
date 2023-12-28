15d088e55e8efab203fcb912bd6b119979e9973c
haecheol-shin <dev.hc.shin@gmail.com>
2023-12-28 21:41:17 +0900

    Procfile added

--- src/main/java/toyproject/boardcrud/BoardCrudApplication.java
+++ src/main/java/toyproject/boardcrud/BoardCrudApplication.java
@@ -3,11 +3,19 @@ package toyproject.boardcrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class BoardCrudApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BoardCrudApplication.class, args);
+
+		Path path = Paths.get("");
+		System.out.println(path.toAbsolutePath().toString());
+
 	}
 
 }
